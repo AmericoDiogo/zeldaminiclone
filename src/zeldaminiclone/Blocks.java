5f8edc3bdd421aa5d642c69c39688dae46744d00
Diogo Américo <diogoamericonunes14@gmail.com>
2021-10-03 20:50:04 -0300

    certo

---
+++ src/zeldaminiclone/Blocks.java
@@ -0,0 +1,24 @@
+package zeldaminiclone;
+
+import java.awt.Rectangle;
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class Blocks extends Rectangle{
+
+	public Blocks(int x, int y) {
+		super(x,y,32,32);
+	}
+	
+	public void render(Graphics g) {
+		
+		//g.setColor(Color.MAGENTA);
+		//g.fillRect(x,y,width,height);
+		//g.setColor(Color.black);
+		//g.drawRect(x,y,width,height);
+		
+		g.drawImage(Spritesheet.tileWall, x,y,32,32,null);
+		
+	}
+	
+}
---
+++ src/zeldaminiclone/Spritesheet.java
@@ -0,0 +1,40 @@
+package zeldaminiclone;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Spritesheet {
+	
+	public static BufferedImage spritesheet;
+	
+	public static BufferedImage[] player_front;
+	
+	public static BufferedImage[] inimigo_front;
+	
+	public static BufferedImage tileWall;
+	
+	public Spritesheet() {
+		try {
+			spritesheet = ImageIO.read(getClass().getResource("/aula05-spritesheet.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		player_front = new BufferedImage[2];
+		inimigo_front = new BufferedImage[2];
+		
+		player_front[0] = Spritesheet.getSprite(0, 11, 16, 16);
+		player_front[1] = Spritesheet.getSprite(17, 11, 16, 16);
+		
+		inimigo_front[0] = Spritesheet.getSprite(128, 224, 16, 16);
+		inimigo_front[1] = Spritesheet.getSprite(146, 224, 16, 16);
+
+		tileWall = Spritesheet.getSprite(316, 185, 16, 16);
+	}
+	
+	public static BufferedImage getSprite(int x, int y, int width, int height) {
+		return spritesheet.getSubimage(x,y,width,height);
+	}
+
+}
